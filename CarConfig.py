288e184b2e9e6b623c7ab74b07b6d04b1b89fcf4
PALLAVI MITRA <pallavi.mitra1992@gmail.com>
2021-03-04 14:18:37 +0100

    Add files via upload

---
+++ CarConfig.py
@@ -0,0 +1,42 @@
+
+import shutil, os
+import numpy as np
+import pandas as pd
+from scipy import misc
+import imageio
+
+ModelsPath = "KerasModels/"
+DataPath = "data/Synthetic"
+RBGMode = True                 #If false, recorded data shall be Gray
+ConvFolder2Gray = False        #If True, recorded RGB data is converted to Gray
+
+#Number of images to stack and train (memory)
+#Shall be Either 4 or 0 (1 same as 0) else keras model in CarRacing_Learn shall be updated either
+
+Temporal_Buffer = 4 
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+
+def save_data(Path, action_l, reward_l, state_l):
+    ''' Saves actions, rewards and states (images) in DataPath'''
+    if not os.path.exists(Path):
+        os.makedirs(Path)
+    else:
+        shutil.rmtree(Path)
+        os.makedirs(Path)
+
+    df = pd.DataFrame(action_l, columns=["Steering", "Throttle", "Brake"])
+    df["Reward"] = reward_l
+    df.to_csv(Path+'/CarRacing_ActionsRewards.csv', index=False)
+
+    #img = np.empty(len(state_l))
+    for i in range(len(state_l)):
+        if RBGMode == False:
+            image = rgb2gray(state_l[i])
+        else:
+            image = state_l[i]
+        ##misc.imsave("d7ata/Img"+str(i)+".png", state_l[i])
+        imageio.imwrite(Path+"/Img"+str(i)+".png", image)
